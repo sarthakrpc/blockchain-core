5112be613fe4771364664e1cdcabfd056e69e7cf
Sarthak <sarthakkumar766@gmail.com>
2022-06-25 21:12:52 +0530

    blockHash create

--- src/main.ts
+++ src/main.ts
@@ -1,32 +1,27 @@
-import { pushTxtoMemPool, memPoolArray } from "./mempool";
-import { Transaction } from "./Transaction";
-import crypto from "crypto";
+import { pushTxtoMemPool } from "./mempool";
+import mainExec from "./executeTxn";
 
-const buildHash = (value: string) => {
-  const hashedValue: string = crypto
-    .createHash("sha256")
-    .update(value)
-    .digest("hex");
-  return hashedValue;
+interface data {
+  addModule: string;
+  from: string;
+  values: object;
+}
+
+interface values {
+  data: data;
+  time: string;
+}
+
+const sendTransactionParams = (val: values, sentTime: string) => {
+  let incomingTx: object = val;
+  val.time = sentTime;
+  pushTxtoMemPool(incomingTx);
+  myFunc();
 };
 
-const sendTransactionParams = (val: object, sentTime: string) => {
-  let incomingTx: object = val;
-  pushTxtoMemPool(incomingTx);
-
-  memPoolArray.forEach((element: any) => {
-    // console.log(element.data);
-    for (const key in element.data.values) {
-      const newTx = new Transaction();
-      newTx.from = element.data.from;
-      newTx.addModule = element.data.addModule;
-      //   console.log(key);
-      //   console.log(element.data.values[key]);
-      newTx.sentTime = sentTime;
-      const hashedVal: string = buildHash(element.data.values[key]);
-	//   console.log(hashedVal);
-    }
-  });
+let myFunc = () => {
+  myFunc = function () {}; // kill it as soon as it was called
+  mainExec();
 };
 
 export { sendTransactionParams };
