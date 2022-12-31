2ddc8d5313be6a021fdeb5ae684eaf376deed73d
sarthakkumar766 <sarthakkumar766@gmail.com>
2022-12-31 18:18:56 +0530

    add comment for better request handling

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,19 @@
 import express, { Express } from "express";
 const cors = require("cors");
-import {sendTransactionParams} from "./main"
+import { sendTransactionParams } from "./main";
 const app: Express = express();
 app.use(cors());
 app.use(express.json());
+
 app.post("/", (req: any, res: any) => {
-//   console.log(req.body);
+  // Expected data format :- { "data" : { "values": { "key1": "value_1", "key2": "value_2"}}}
+  //  console.log(req.body);
+
   res.send(JSON.stringify({ body: req.body }));
-  const sentTime:string =(new Date().getTime()/1000).toFixed(0);
+  const sentTime: string = (new Date().getTime() / 1000).toFixed(0);
   sendTransactionParams(req.body, sentTime);
 });
 
 app.listen(8000, () => {
   console.log("Server started on port 8000");
 });
-
-
